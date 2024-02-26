66c7f03f170447bf4377eb97eb0803e046af507a
darkfrozen132 <darkfrozen@outlook.com>
2024-02-25 21:12:28 -0500

    asd

--- Restaurante/obj/Debug/net8.0/Restaurante.AssemblyInfo.cs
+++ Restaurante/obj/Debug/net8.0/Restaurante.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a288279aab1f54dccbe8de7ff8fb90434953096c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
